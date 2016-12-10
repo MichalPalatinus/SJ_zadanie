3599b9194263bb72493cde9a863f201d287e3014
akulisek <akulisek@gmail.com>
2016-12-10 17:52:07 +0100

    Tokenizer fix for chars * + ?

--- parser.py
+++ parser.py
@@ -27,9 +27,9 @@ if input_string:
 # OK <!ATTLIST :merry^Christmas&PF%2017 Santa:Claus NMTOKEN #IMPLIED Jingl_Bells IDREF #REQUIRED>
 # OK <!ELEMENT Morho-detvo-mojho-rodu ANY>
 # OK <!ELEMENT from (#PCDATA)> <!ATTLIST Ahoj hulahej IDREF #REQUIRED>
-# OK <!ELEMENT I_LOVE_FIIT^^ ( ( or_not ? ) )>
-# OK <!ELEMENT Winter.is.coming. ( ( ( valar * | dohaeris * ) + ) )>
-# OK <!ATTLIST Waar kom ( je | u ) #FIXED \" vandaan \">
-# OK <!ATTLIST Strc _2_prsty ( skrz ) \" krk \">
-# OK <!ELEMENT integer ( ( bool , string , float ) * )>
-# "<!ATTLIST Ahoj bla1_D CDATA #REQUIRED> !ELEMENT _ahoj-cau. EMPTY> <!ATTLIST :merry^Christmas&PF%2017 Santa:Claus NMTOKEN #IMPLIED Jingl_Bells IDREF #REQUIRED> <!ELEMENT Morho-detvo-mojho-rodu ANY> <!ELEMENT from (#PCDATA)> <!ATTLIST Ahoj hulahej IDREF #REQUIRED> <!ELEMENT I_LOVE_FIIT^^ ( ( or_not ? ) )> <!ATTLIST Waar kom ( je | u ) #FIXED \" vandaan \"> <!ATTLIST Strc _2_prsty ( skrz ) \" krk \"> <!ELEMENT integer ( ( bool , string , float ) * )>"
+# OK <!ELEMENT I_LOVE_FIIT^^ ((or_not?))>
+# OK <!ELEMENT Winter.is.coming. (((valar*|dohaeris*)+))>
+# OK <!ATTLIST Waar kom (je|u) #FIXED \"vandaan\">
+# OK <!ATTLIST Strc _2_prsty (skrz) #FIXED \"krk\">
+# OK <!ELEMENT integer ((bool,string,float)*)>
+# "<!ATTLIST Ahoj bla1_D CDATA #REQUIRED> !ELEMENT _ahoj-cau. EMPTY> <!ATTLIST :merry^Christmas&PF%2017 Santa:Claus NMTOKEN #IMPLIED Jingl_Bells IDREF #REQUIRED> <!ELEMENT Morho-detvo-mojho-rodu ANY> <!ELEMENT from (#PCDATA)> <!ATTLIST Ahoj hulahej IDREF #REQUIRED> <!ELEMENT I_LOVE_FIIT^^ ((or_not?))> <!ATTLIST Waar kom (je|u) #FIXED \"vandaan\"> <!ATTLIST Strc _2_prsty (skrz) \"krk\"> <!ELEMENT integer ((bool,string,float)*)>"
