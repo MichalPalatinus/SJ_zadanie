42abd21a24defda45a8bb69f922543b49fadcbb0
akulisek <akulisek@gmail.com>
2016-12-11 18:25:23 +0100

    Resolve merge conflict

--- parser.py
+++ parser.py
@@ -4,11 +4,11 @@ import argparse
 from token import Token
 
 parser = argparse.ArgumentParser(description="Parser of basicDTD language")
-parser.add_argument('--input', help='Text to be parsed', type=str)
+parser.add_argument('--input', help='text to be parsed in single quotes (supports multiple DTD sentences delimited by whitespace)', type=str)
 args = parser.parse_args()
 
 input_string = args.input
-#input_string = "<!ATTLIST Ahoj bla1_D CDATA #REQUIRED> <!ELEMENT Winter.is.coming. (((valar*|dohaeris*)+))> <!ATTLIST Waar kom (je|u) #FIXED \"vandaan :Alstublieft0\"> <!ELEMENT _ahoj-cau. EMPTY> <!ATTLIST :merry^Christmas&PF%2017 Santa:Claus NMTOKEN #IMPLIED Jingl_Bells IDREF #REQUIRED> <!ELEMENT Morho-detvo-mojho-rodu ANY> <!ELEMENT from (#PCDATA)> <!ATTLIST Ahoj hulahej IDREF #REQUIRED> <!ELEMENT I_LOVE_FIIT^^ ((or_not?))> <!ATTLIST Waar kom (je|u) #FIXED \"vandaan\"> <!ATTLIST Strc _2_prsty (skrz) \"krk\"> <!ELEMENT integer ((bool,string,float)*)>"
+
 if input_string:
     print "\tYou have specified this as an input: \n" + input_string + '\n'
     tokenizer = Tokenizer()
